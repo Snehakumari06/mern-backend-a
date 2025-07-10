9b89b02d7c5c7d7b67eb64d27fa3cf8c0c871339
Snehakumari06 <snehasingh5754@gmail.com>
2025-07-10 11:53:39 +0530

    update

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+
+
+app.use(express.static("public"));    //jo bhi file hai wo isme wo return karega
+app.use(cors()); // able to call apis
+
+
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose
-//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
-//   .then(() => {
-//     app.listen(8080, () => {
-//       console.log("Server started");
-//     });
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
 //   });
+// });
 
   mongoose
   .connect(`mongodb+srv://${dbuser}:${dbpass}@sneha.tbgpj9n.mongodb.net/merndb?retryWrites=true&w=majority&appName=Sneha`)
